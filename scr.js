358ffb73f0028a9a7105d4d01395cf36996c36c4
James <jamesaubudi@gmail.com>
2019-02-08 18:00:21 -0800

    implemented drag resize function

--- scr.js
+++ scr.js
@@ -1,21 +1,27 @@
+/*** Global variables ***/
+
 var isDown = false; // global state to check if an image is being dragged or not;
 var isResizing = false;
+var currentTarget = null;
+var currentResizer = null;
 var images = []; // Array to store all image in images, used in reOrganize function
 var initialWidth = 0;
-var intialHeight = 0;
+var initialHeight = 0;
 var initialMouseX = 0;
 var initialMouseY = 0;
 var initialX = 0;
 var initialY = 0;
 
+/************************/
+
 function appendLeftTab(file, imageWrapper) {
-  var leftTab = document.getElementById("left");
-  var delBtn = document.createElement("button");
+  const leftTab = document.getElementById("left");
+  const delBtn = document.createElement("button");
   delBtn.textContent = "X";
   delBtn.className = "btn--del";
   // Add onclick event to the delete button
-  delBtn.onclick = e => {
-    var target = document.getElementById(imageWrapper.lastChild.id);
+  delBtn.onclick = (e) => {
+    const target = imageWrapper;
     // Update the local images array for the re-organize function
     images = images.filter(
       img => img.lastChild.id !== imageWrapper.lastChild.id
@@ -23,10 +29,10 @@ function appendLeftTab(file, imageWrapper) {
     // e.target is the button, to get to the paragraph, we have to get e.target.parentNode
     leftTab.removeChild(e.target.parentNode);
     // Next, we remove the target image's wrapper (so the image is gone too)
-    target.parentNode.parentNode.removeChild(target.parentNode);
+    target.parentNode.removeChild(target);
   };
-  var fileNameParagraph = document.createElement("p");
-  var fileName = document.createTextNode(file.name);
+  const fileNameParagraph = document.createElement("p");
+  const fileName = document.createTextNode(file.name);
   fileNameParagraph.addEventListener("mouseover", function() {
     imageWrapper.style.border = "10px solid #42f48c";
     imageWrapper.style.zIndex = 100;
@@ -40,69 +46,125 @@ function appendLeftTab(file, imageWrapper) {
   leftTab.append(fileNameParagraph);
 }
 
-function createResizer(resizer, imageWrapper) {
+function createResizer(resizer, imageWrapper, resizeFunction) {
   resizer.addEventListener("mousedown", e => {
     e.preventDefault();
-    initialWidth = parseFloat(getComputedStyle(imageWrapper, null).getPropertyValue('width').replace('px', ''));
-    initialHeight = parseFloat(getComputedStyle(imageWrapper, null).getPropertyValue('height').replace('px', ''));
-    initialX = imageWrapper.getBoundingClientRect().left;
-    initialY = imageWrapper.getBoundingClientRect().top;
+    isDown = false;
+    isResizing = true;
+    currentTarget = imageWrapper;
+    currentResizer = e.target;
+    initialWidth = parseFloat(
+      getComputedStyle(currentTarget, null)
+        .getPropertyValue("width")
+        .replace("px", "")
+    );
+    initialHeight = parseFloat(
+      getComputedStyle(currentTarget, null)
+        .getPropertyValue("height")
+        .replace("px", "")
+    );
+    initialX = currentTarget.getBoundingClientRect().left;
+    initialY = currentTarget.getBoundingClientRect().top;
     initialMouseX = e.pageX;
     initialMouseY = e.pageY;
-    window.addEventListener("mousemove", resize(e), true);
+    window.addEventListener("mousemove", resizeFunction);
     window.addEventListener("mouseup", stopResize);
   });
 }
 
-function resize(e) {
-  // if (bottomRightResizer.classList.contains("bottom-right")) {
-  //   // imageWrapper.style.width = e.pageX - imageWrapper.getBoundingClientRect().left + "px";
-  // } else 
-  var imageWrapper = document.querySelector('.wrapper-image');
-  console.log(imageWrapper);
-  if (bottomLeftResizer.classList.contains("bottom-left")) {
-    imageWrapper.style.width = initialWidth - (e.pageX - initialMouseX) + 'px';
-    imageWrapper.style.left = initialX + (e.pageX - initialMouseX) + 'px';
+function bottomRightResize(e) {
+  /* Note: e.pageX = X of the current cursor.
+          e. pageY = Y of the current cursor.
+          initialMouseX = original X of the cursor, stored when resizer is clicked
+          initialMouseY = original Y of the cursor, stored when resizer is clicked
+          initialWidth = original boundingX
+          initialHeight = original boundingY
+  */
+  if (isResizing && currentResizer.classList.contains("bottom-right")) {
+    currentTarget.style.width = initialWidth + (e.pageX - initialMouseX) + "px";
+    currentTarget.style.height =
+      initialHeight + (e.pageY - initialMouseY) + "px";
+  }
+}
+
+function bottomLeftResize(e) {
+  if (isResizing && currentResizer.classList.contains("bottom-left")) {
+    currentTarget.style.width = initialWidth - (e.pageX - initialMouseX) + "px";
+    currentTarget.style.height =
+      initialHeight + (e.pageY - initialMouseY) + "px";
+    currentTarget.style.left = initialX + (e.pageX - initialMouseX) + "px";
+  }
+}
+
+function topLeftResize(e) {
+  if (isResizing && currentResizer.classList.contains("top-left")) {
+    currentTarget.style.width = initialWidth - (e.pageX - initialMouseX) + "px";
+    currentTarget.style.height =
+      initialHeight - (e.pageY - initialMouseY) + "px";
+    currentTarget.style.left = initialX + (e.pageX - initialMouseX) + "px";
+    currentTarget.style.top = initialY + (e.pageY - initialMouseY) + "px";
+  }
+}
+
+function topRightResize(e) {
+  if (isResizing && currentResizer.classList.contains("top-right")) {
+    currentTarget.style.width = initialWidth + (e.pageX - initialMouseX) + "px";
+    currentTarget.style.height =
+      initialHeight - (e.pageY - initialMouseY) + "px";
+    currentTarget.style.top = initialY + (e.pageY - initialMouseY) + "px";
   }
 }
 
 function stopResize(e) {
-  initialWidth = 0;
-  initialHeight = 0;
-  initialMouseX = 0;
-  initialMouseY = 0;
-  window.removeEventListener("mousemove", resize);
+  isResizing = false;
+  currentTarget = null;
+  currentResizer = null;
+  initialWidth = null;
+  initialHeight = null;
+  initialMouseX = null;
+  initialMouseY = null;
+  initialX = null;
+  initialY = null;
+  window.removeEventListener("mousemove", bottomRightResize);
+  window.removeEventListener("mousemove", bottomLeftResize);
 }
 
 function makeImage(reader, preview, file) {
-  var image = new Image();
-  var imageWrapper = document.createElement("div");
+  const image = new Image();
+  const imageWrapper = document.createElement("div");
   imageWrapper.className = "wrapper-image";
-  var main = document.querySelector(".main");
-  var upload = document.getElementById("upload");
+  const main = document.querySelector(".main");
+  const upload = document.getElementById("upload");
   // Default width is the main workspace's width divided by 4 (aka can fit 4 images)
-  var defaultWidth = main.clientWidth / 4;
-  var resizers = document.createElement("div");
-  var topLeftResizer = document.createElement("div");
-  var topRightResizer = document.createElement("div");
-  var bottomLeftResizer = document.createElement("div");
-  var bottomRightResizer = document.createElement("div");
+  const defaultWidth = main.clientWidth / 4;
+  const defaultHeight = main.clientHeight / 3;
+  const resizers = document.createElement("div");
+  const topLeftResizer = document.createElement("div");
+  const topRightResizer = document.createElement("div");
+  const bottomLeftResizer = document.createElement("div");
+  const bottomRightResizer = document.createElement("div");
   resizers.className = "resizers";
   topLeftResizer.className = "resizer top-left";
   topRightResizer.className = "resizer top-right";
   bottomLeftResizer.className = "resizer bottom-left";
   bottomRightResizer.className = "resizer bottom-right";
-  createResizer(bottomRightResizer, imageWrapper);
-  createResizer(bottomLeftResizer, imageWrapper)
+  createResizer(bottomRightResizer, imageWrapper, bottomRightResize);
+  createResizer(bottomLeftResizer, imageWrapper, bottomLeftResize);
+  createResizer(topRightResizer, imageWrapper, topRightResize);
+  createResizer(topLeftResizer, imageWrapper, topLeftResize);
   resizers.appendChild(topLeftResizer);
   resizers.appendChild(topRightResizer);
   resizers.appendChild(bottomLeftResizer);
   resizers.appendChild(bottomRightResizer);
   imageWrapper.append(resizers);
   image.className = "image";
+  resizers.width = defaultWidth;
+  resizers.height = defaultHeight;
   image.width = defaultWidth;
+  image.height = defaultHeight;
   image.title = file.name;
   imageWrapper.style.width = image.width + "px";
+  imageWrapper.style.height = image.height + "px";
   // The resulting file data
   image.src = reader.result;
   imageWrapper.addEventListener(
@@ -117,6 +179,10 @@ function makeImage(reader, preview, file) {
   imageWrapper.ondragstart = function() {
     return false;
   };
+  image.ondragstart = function() {
+    return false;
+  };
+
   imageWrapper.addEventListener(
     "mousemove",
     function(e) {
@@ -143,7 +209,8 @@ function makeImage(reader, preview, file) {
   imageWrapper.addEventListener("mouseout", function() {
     imageWrapper.style.zIndex = 1;
   });
-  image.id = images.length.toString();
+  // Create an id for imagewrapper as an identifier later
+  imageWrapper.id = images.length.toString();
   // Add image into the image wrapper
   imageWrapper.appendChild(image);
   // Add the whole imageWrapper div into the preview div
@@ -154,13 +221,13 @@ function makeImage(reader, preview, file) {
 }
 
 function previewFile() {
-  var preview = document.getElementById("upload"); // Selects query with #upload
-  var files = document.querySelector("input[type=file]").files; //sames as here
+  const preview = document.getElementById("upload"); // Selects query with #upload
+  const files = document.querySelector("input[type=file]").files; //sames as here
 
   function readAndPreview(file) {
     // Make sure `file.name` matches our extensions criteria
     if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
-      var reader = new FileReader();
+      const reader = new FileReader();
 
       reader.addEventListener(
         "load",
@@ -179,14 +246,14 @@ function previewFile() {
 }
 
 function reOrganize() {
-  var offsetLeft = 0;
-  var offsetTop = 0;
-  var main = document.querySelector(".main");
+  let offsetLeft = 0;
+  let offsetTop = 0;
+  const main = document.querySelector(".main");
   images.forEach(imageWrapper => {
     // Left tab is 10% of vw + it's padding
-    var left = window.innerWidth * 0.1 - 10;
+    let left = window.innerWidth * 0.1 - 10;
     // Top is height of the margin-bottom-20px div
-    var top = 100;
+    let top = 100;
     console.log(imageWrapper);
     // Increment offset each time an image is added, so that images will not overlap
     imageWrapper.style.left = left + offsetLeft + "px";
